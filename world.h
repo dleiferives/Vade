4041252e936c295375a222958bcf2583337c34f8
dleiferives <dleiferives@gmail.com>
2021-11-14 19:01:34 -0800

    automated commit with comp

--- world.h
+++ world.h
@@ -239,7 +239,7 @@ struct level * generate_level_structure(int id, int diff)
 	int diff_width = pow(2.7, ((float)diff/(float)2) ) + 19;
 	int diff_height = pow(2.7, ((float)diff/(float)2) )+ 19;
 	int diff_mobs = ((double) diff_width * (double) diff_height) * ((double)1.0- ((double) (((double)10.0)/((double) diff + (double) 10.0))));
-	int diff_rooms = pow(2.6, ((float)diff/(float)2) ) + 8;
+	int diff_rooms = pow(2.5, ((float)diff/(float)2) ) + 8;
 	levels[id] = gen_level(id,diff, diff_width, diff_height, diff_mobs);
 	for(int i=0; i < diff_width * diff_height; i++)
 		levels[id].map[i] = '.';
