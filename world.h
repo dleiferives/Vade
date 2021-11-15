8b56e9f89364b5a5be04f047cdf2c9b0c2b64d52
dleiferives <dleiferives@gmail.com>
2021-11-15 01:30:20 -0800

    automated commit with comp

--- world.h
+++ world.h
@@ -271,10 +271,7 @@ struct pos generate_level_structure(int id, int diff)
 	seed_rand();
 	for(int j=0; j< diff_rooms;j++)
 	{
-		for(int i=0;i<20; i++)
-		{
 			gen_rand_room(&levels[id]);	
-		}
 		normalize_level(&levels[id]);
 	}
 	normalize_level(&levels[id]);
