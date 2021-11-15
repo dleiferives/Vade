da6b699a363ec30e2da3ae9cfd01f9e43d413c2a
dleiferives <dleiferives@gmail.com>
2021-11-14 21:38:29 -0800

    automated commit with comp

--- Vade.ino
+++ Vade.ino
@@ -92,19 +92,27 @@ int _main()
 				if(temp_dir == 2)
 				{
 					next_level(&player);
+					puts("1");
 				}
+					puts("8");
 				break;
 
+					puts("7");
 		}
 		// runs if the time has advanced. time does not 
+					puts("6");
 		if(g_time > old_g_time)
 		{
 			/* run entities code */
 		}
 
+					puts("5");
 		render_character(&player);
+					puts("4");
 		get_render_map();
+					puts("3");
 		print_map(levels[cur_level].lcd);
+					puts("2");
 
 	}
 	
